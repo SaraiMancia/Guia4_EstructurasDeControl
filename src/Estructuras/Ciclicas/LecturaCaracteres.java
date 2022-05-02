c9ff6558781a2b1336734d03eeb2ff3578d0a536
Melani05 <melanibeltran68@gmail.com>
2022-05-02 15:20:21 -0300

    ejemplo 3

---
+++ src/Estructuras/Ciclicas/LecturaCaracteres.java
@@ -0,0 +1,28 @@
+package Estructuras.Ciclicas;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class LecturaCaracteres {
+    
+    public static void main(String[] args) {
+      try{
+          BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+          System.out.println("Ingresar el nombre: ");
+          String nombre = leer.readLine();
+          
+          leerCaracteresFor(nombre.toUpperCase());
+      }catch(Exception e){
+          System.out.println(e.getMessage());
+      }
+    }
+    
+    public static void leerCaracteresFor(String palabra){
+        
+    for(int i = 0; i < palabra.length(); i++){
+        System.out.println("Caracter: "+ i + "->: " + palabra.charAt(i));
+    }
+    
+}
+    
+}
